e9eb09f0c7e248d42512060623080e1ae5493134
zspam1 <zspam1@gmail.com>
2018-01-10 04:31:15 -0800

    add ios form

---
+++ myIOSForm.UITests/Tests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.iOS;
+using Xamarin.UITest.Queries;
+
+namespace myIOSForm.UITests
+{
+    [TestFixture]
+    public class Tests
+    {
+        iOSApp app;
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            app = ConfigureApp.iOS.StartApp();
+        }
+
+        [Test]
+        public void ClickAddButtonAddsOneItem()
+        {
+            Func<AppQuery, AppQuery> addButton = c => c.Button("addButton");
+            Func<AppQuery, AppQuery> tableCells = c => c.Class("UITableViewCell");
+            Func<AppQuery, AppQuery> detailView = c => c.Marked("Detail view content goes here");
+            Func<AppQuery, AppQuery> masterButton = c => c.Button("Master");
+
+            AppResult[] results = app.Query(detailView);
+            if (results.Any())
+            {
+                // Detail view is show first by iPad.
+                app.Tap(masterButton);
+            }
+            app.Tap(addButton);
+            results = app.WaitForElement(tableCells);
+            app.Screenshot("New item added.");
+
+            Assert.AreEqual(1, results.Length);
+        }
+    }
+}
+
